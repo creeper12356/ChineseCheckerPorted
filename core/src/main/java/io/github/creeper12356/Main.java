31ba283870939ca0d4c30e83e0c59039954b6e31
creeper <creeperhjt@sjtu.edu.cn>
2024-08-20 00:00:11 +0800

    feat: 完成故事模式菜单布局

--- core/src/main/java/io/github/creeper12356/Main.java
+++ core/src/main/java/io/github/creeper12356/Main.java
@@ -3,6 +3,7 @@ package io.github.creeper12356;
 import com.badlogic.gdx.Game;
 
 import io.github.creeper12356.screen.MainMenuScreen;
+import io.github.creeper12356.screen.StoryMenuScreen;
 import io.github.creeper12356.screen.TitleScreen;
 import io.github.creeper12356.utils.Resource;
 
@@ -10,7 +11,7 @@ public class Main extends Game {
 
     @Override
     public void create() {
-        setScreen(new MainMenuScreen());
+        setScreen(new StoryMenuScreen());
     }
 
     @Override
--- core/src/main/java/io/github/creeper12356/screen/StoryMenuScreen.java
+++ core/src/main/java/io/github/creeper12356/screen/StoryMenuScreen.java
@@ -9,94 +9,84 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Scaling;
 
 import io.github.creeper12356.utils.Resource;
 
-public class MainMenuScreen implements Screen {
+public class StoryMenuScreen implements Screen {
+
     private Stage stage;
     private Table table;
 
     private Texture imgBackground;
-    private Texture imgBackgroundMenu;
+    private Texture imgTextNew;
+    private Texture imgTextContinue;
 
-    private Texture[] imgMenuTextUp = new Texture[7];
-    private Texture[] imgMenuTextDown = new Texture[7];
+    private Texture imgTouchButtonBackground;
+    private Texture imgTouchButtonCancel;
 
-    public MainMenuScreen() {
+    public StoryMenuScreen() {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
-
         imgBackground = Resource.loadImage("background.png");
         Image img = new Image(imgBackground);
         img.setFillParent(true);
         stage.addActor(img);
 
-        imgBackgroundMenu = Resource.loadImage("bgmenu.png");
-        Image imgMenu = new Image(imgBackgroundMenu);
-        imgMenu.setPosition(0, Gdx.graphics.getHeight(), Align.topLeft);
-        stage.addActor(imgMenu);
-
         table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
 
-        for (int i = 0; i < 7; ++i) {
-            imgMenuTextUp[i] = Resource.loadImage("menutext_1" + i + ".png");
-            imgMenuTextDown[i] = Resource.loadImage("menutext_2" + i + ".png");
-        }
+        imgTextNew = Resource.loadImage("menutext_31.png");
+        imgTextContinue = Resource.loadImage("menutext_32.png");
 
-        addImageButton(imgMenuTextUp[0], imgMenuTextDown[0], new ClickListener() {
+        addImageButton(imgTextNew, imgTextNew, new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Play");
+                System.out.println("New");
             }
         });
 
-        addImageButton(imgMenuTextUp[1], imgMenuTextDown[1], new ClickListener() {
+        addImageButton(imgTextContinue, imgTextContinue, new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Rank");
+                System.out.println("Continue");
             }
         });
 
-        addImageButton(imgMenuTextUp[2], imgMenuTextDown[2], new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Story");
-            }
-        });
+        imgTouchButtonBackground = Resource.loadImage("touch/tbg.png");
+        imgTouchButtonCancel = Resource.loadImage("touch/tc.png");
 
-        addImageButton(imgMenuTextUp[3], imgMenuTextDown[3], new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Option");
-            }
-        });
+        Stack stack = new Stack();
+        Image image = new Image(imgTouchButtonCancel);
+        image.setScaling(Scaling.fit);
+        stack.add(image);
 
-        addImageButton(imgMenuTextUp[4], imgMenuTextDown[4], new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Help");
-            }
-        });
 
-        addImageButton(imgMenuTextUp[5], imgMenuTextDown[5], new ClickListener() {
+        // TODO: 抽象出右下角按钮
+        ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+        style.up = new TextureRegionDrawable(new TextureRegion(imgTouchButtonBackground));
+        ImageButton cancelButton = new ImageButton(style);
+        cancelButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                System.out.println("About");
+                System.out.println("Cancel");
             }
         });
+        stack.add(cancelButton);
 
-        addImageButton(imgMenuTextUp[6], imgMenuTextDown[6], new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Exit");
-            }
-        });
+        Table cancelTable = new Table();
+        cancelTable.setFillParent(true);
+        cancelTable.bottom().right();
+        cancelTable.add(stack);
+
+        stage.addActor(cancelTable);
+        
 
     }
 
@@ -116,10 +106,12 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void pause() {
+
     }
 
     @Override
     public void resume() {
+
     }
 
     @Override
@@ -128,15 +120,12 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
-        stage.dispose();
-
         imgBackground.dispose();
-        imgBackgroundMenu.dispose();
+        imgTextNew.dispose();
+        imgTextContinue.dispose();
 
-        for (int i = 0; i < 7; ++i) {
-            imgMenuTextUp[i].dispose();
-            imgMenuTextDown[i].dispose();
-        }
+        imgTouchButtonBackground.dispose();
+        imgTouchButtonCancel.dispose();
     }
 
     private void addImageButton(Texture upTexture, Texture downTexture, EventListener listener) {
